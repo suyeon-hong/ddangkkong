1e95bbb3b274fdaf3cab2b85bc812c26857d8a9d
suyeon-hong <bellahong95@gmail.com>
2022-03-06 16:48:44 +0900

    :bento: Assets: 이미지 파일 추가, 삭제

---
+++ public/images/index.ts
@@ -0,0 +1,7 @@
+export { default as check1 } from "./check1.gif";
+export { default as check2 } from "./check2.gif";
+export { default as check3 } from "./check3.gif";
+export { default as book1 } from "./book1.png";
+export { default as book2 } from "./book2.png";
+export { default as book3 } from "./book3.png";
+export { default as liveReading } from "./liveReading.png";
